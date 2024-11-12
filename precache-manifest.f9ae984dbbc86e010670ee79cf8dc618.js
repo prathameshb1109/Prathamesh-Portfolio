d732e75db0efddce8dd102e8234737f23e97de45
Prathamesh Bhosale <prathameshvelocity5@gmail.com>
2024-11-12 16:04:37 +0530

    Updates

---
+++ precache-manifest.f9ae984dbbc86e010670ee79cf8dc618.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9ef1b2338b33daefd3e9b7eeb23e54",
+    "url": "/Prathamesh-Portfolio/index.html"
+  },
+  {
+    "revision": "06502abec84366d92617",
+    "url": "/Prathamesh-Portfolio/static/css/2.65802816.chunk.css"
+  },
+  {
+    "revision": "df3eea196f034264a7fc",
+    "url": "/Prathamesh-Portfolio/static/css/main.2be1ea85.chunk.css"
+  },
+  {
+    "revision": "06502abec84366d92617",
+    "url": "/Prathamesh-Portfolio/static/js/2.da88064a.chunk.js"
+  },
+  {
+    "revision": "e9d9b058df1e97b30148d7042082faa1",
+    "url": "/Prathamesh-Portfolio/static/js/2.da88064a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a779f343d7e9d5c017ce",
+    "url": "/Prathamesh-Portfolio/static/js/3.9762f19f.chunk.js"
+  },
+  {
+    "revision": "df3eea196f034264a7fc",
+    "url": "/Prathamesh-Portfolio/static/js/main.87313be6.chunk.js"
+  },
+  {
+    "revision": "2590cc7aec4805031049",
+    "url": "/Prathamesh-Portfolio/static/js/runtime-main.7ed65ce5.js"
+  },
+  {
+    "revision": "730354a28cdb32c8b3cffde060cc787c",
+    "url": "/Prathamesh-Portfolio/static/media/alemik-digital.730354a2.jpg"
+  },
+  {
+    "revision": "af14a7a165d436a506c6997f95574481",
+    "url": "/Prathamesh-Portfolio/static/media/alemik-digital.af14a7a1.png"
+  },
+  {
+    "revision": "0cc9ece76c2a460638079aa8575d4e50",
+    "url": "/Prathamesh-Portfolio/static/media/brilliant-care.0cc9ece7.png"
+  },
+  {
+    "revision": "91baaa97d22051a9e079e9f4d66fd7c9",
+    "url": "/Prathamesh-Portfolio/static/media/brilliant-care.91baaa97.jpg"
+  },
+  {
+    "revision": "bd59d42549cfdb94c1eec0da8edb6454",
+    "url": "/Prathamesh-Portfolio/static/media/child-vision-trust.bd59d425.png"
+  },
+  {
+    "revision": "e29f5839c5a5555c232b9587efecb4e1",
+    "url": "/Prathamesh-Portfolio/static/media/child-vision-trust.e29f5839.jpg"
+  },
+  {
+    "revision": "404c9d4974395282523ecec36a8ce6c4",
+    "url": "/Prathamesh-Portfolio/static/media/ezephyr.404c9d49.png"
+  },
+  {
+    "revision": "02d0e83252355be2922a552388f20153",
+    "url": "/Prathamesh-Portfolio/static/media/home-bg-two.02d0e832.jpg"
+  },
+  {
+    "revision": "0295d80c99539e33f8d5b97afab52c19",
+    "url": "/Prathamesh-Portfolio/static/media/home-bg.0295d80c.jpg"
+  },
+  {
+    "revision": "e51a8c8e69261ba2aaa58d9433e2fa7a",
+    "url": "/Prathamesh-Portfolio/static/media/html-svg.e51a8c8e.svg"
+  },
+  {
+    "revision": "b9361791b29c7cca6b114c7e78a1954d",
+    "url": "/Prathamesh-Portfolio/static/media/indesignbynatasha.b9361791.png"
+  },
+  {
+    "revision": "e24bd1a80233e1f5cafa18cf8c2395fe",
+    "url": "/Prathamesh-Portfolio/static/media/indesignbynatasha.e24bd1a8.jpg"
+  },
+  {
+    "revision": "03dd845d2d6f1756d537eb74b3d9153c",
+    "url": "/Prathamesh-Portfolio/static/media/krimakay.03dd845d.png"
+  },
+  {
+    "revision": "d0fc9819d1ade748cd5d9ffaf0ef39e2",
+    "url": "/Prathamesh-Portfolio/static/media/krimakay.d0fc9819.jpg"
+  },
+  {
+    "revision": "11fe74b73ebe2f45aa240c0d021c7614",
+    "url": "/Prathamesh-Portfolio/static/media/mastertechaz.11fe74b7.jpg"
+  },
+  {
+    "revision": "81f3f77563d6b071262cc6885ad84893",
+    "url": "/Prathamesh-Portfolio/static/media/mastertechaz.81f3f775.png"
+  },
+  {
+    "revision": "d0a2cc19fec909187ef4512670b622c8",
+    "url": "/Prathamesh-Portfolio/static/media/namti-salon.d0a2cc19.png"
+  },
+  {
+    "revision": "f006e5b1165c42ba97fb69f3a78846f8",
+    "url": "/Prathamesh-Portfolio/static/media/namtisalon.f006e5b1.jpg"
+  },
+  {
+    "revision": "9ef356a17667e92864d8d7918b3c3ddb",
+    "url": "/Prathamesh-Portfolio/static/media/new-ezephyr.9ef356a1.jpg"
+  },
+  {
+    "revision": "e032ea5e1913bd991d7c8343d568c598",
+    "url": "/Prathamesh-Portfolio/static/media/profile-image.e032ea5e.jpg"
+  },
+  {
+    "revision": "94b56c93d6c853bff527e5954cc99ea5",
+    "url": "/Prathamesh-Portfolio/static/media/react-svg.94b56c93.svg"
+  },
+  {
+    "revision": "c5aa457499b1f19eea5a646fd12bcce2",
+    "url": "/Prathamesh-Portfolio/static/media/shopify-svg.c5aa4574.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Prathamesh-Portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Prathamesh-Portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Prathamesh-Portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Prathamesh-Portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "55fbb56612028c5bca2a9a1232b2ccdd",
+    "url": "/Prathamesh-Portfolio/static/media/trinetra-venture.55fbb566.png"
+  },
+  {
+    "revision": "616aa9c9626063238b5a70521ec2aa0d",
+    "url": "/Prathamesh-Portfolio/static/media/trinetraventure.616aa9c9.jpg"
+  },
+  {
+    "revision": "84ed67240ace5e33f424002d81a859c9",
+    "url": "/Prathamesh-Portfolio/static/media/turakhia-optics.84ed6724.png"
+  },
+  {
+    "revision": "ef27177e15c37f342c7e1eb7739a3443",
+    "url": "/Prathamesh-Portfolio/static/media/turakhiaoptics.ef27177e.jpg"
+  },
+  {
+    "revision": "03437168aa9f3ff4a6f65bdad2bb4a5f",
+    "url": "/Prathamesh-Portfolio/static/media/wordpress-svg.03437168.svg"
+  }
+]);
